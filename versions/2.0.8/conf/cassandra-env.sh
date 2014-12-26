9f354facce3202873825f8cdd825d89efe803766
SiryjVyiko <dz@hivext.net>
2014-12-26 18:08:36 +0200

    Update cassandra-env.sh

--- versions/2.0.8/conf/cassandra-env.sh
+++ versions/2.0.8/conf/cassandra-env.sh
@@ -241,7 +241,7 @@ fi
 # Prefer binding to IPv4 network intefaces (when net.ipv6.bindv6only=1). See 
 # http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=6342561 (short version:
 # comment out this entry to enable IPv6 support).
-JVM_OPTS="$JVM_OPTS -Djava.net.preferIPv4Stack=true"
+JVM_OPTS="$JVM_OPTS -Djava.net.preferIPv4Stack=true ${confresult}"
 
 # jmx: metrics and administration interface
 # 
