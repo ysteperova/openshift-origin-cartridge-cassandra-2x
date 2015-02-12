6035f65bcb85983041fa1cc2622bd9a1d70e7e69
SiryjVyiko <dz@hivext.net>
2015-02-12 11:51:44 +0200

    Update reset_password.sh

--- jelastic/scripts/reset_password.sh
+++ jelastic/scripts/reset_password.sh
@@ -11,7 +11,6 @@ GREP=$(which grep);
 #$J_OPENSHIFT_APP_ADM_PASSWORD    ;   Use this varible for your password
 
 function _setPassword() {
-        return 0;
         new_passwd_file=$(mktemp);
         old_passwd_file=$(mktemp);
         cassanra_conf="/opt/repo/versions/2.0.8/conf/cassandra.yaml";
